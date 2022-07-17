2eb2fac0cd9c065d4adec292d96a6564602194ad
Gilberto Junior <juninhossj4@gmail.com>
2022-07-17 00:23:46 -0300

    Iniciando Prática JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`Sua velocidade é ${vel}`);
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade, você foi MULTADO!`)
+}
+console.log(`Dirija sempra usando o cinto de Segurança!`)
