36aa7695d06b6464f6a000b8f2b50308efe0a0e6
Gilberto Junior <juninhossj4@gmail.com>
2022-07-22 09:59:28 -0300

    Pratica em Java

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,26 @@
-alert('Olá')
+function carregar() {
+
+var msg = window.document.getElementById('msg');
+var img = window.document.getElementById('imagem');
+var data = new Date();
+var hora = data.getHours();
+
+
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora <12){
+    //BOM DIA
+    img.src = './ex014/fotomanha.png';
+    document.body.style.background =  "#e2cd9f";
+} else if (hora >= 12 && hora <= 18){
+
+    //BOA TARDE
+    img.src = './ex014/fototarde.png';
+    document.body.style.background = '#b9846f';
+} else {
+    //BOA NOITE
+    img.src = './ex014/fotonoite.png'
+    document.body.style.background = '#515154'
+    }
+
+}
+
