665dd923ec6ac64b69d652eb02473bddf8942c52
Gilberto Junior <juninhossj4@gmail.com>
2022-08-08 23:47:51 -0300

    Pratica com Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+/*console.log ('Olá')
+console.log ('Olá')
+console.log ('Olá')
+*/
+
+/*
+c = 1;
+while (c <= 6){
+    console.log('ola')
+    c++
+}
+*/
+c = 1;
+
+do{
+    console.log(`Ola ${c}`)
+    c++
+} while(c <=6)
